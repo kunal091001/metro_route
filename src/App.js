9be61fcabf23341699d5cc1d34ed361aa9399513
[kunal] <kunal9313882818@gmail.com>
2023-01-27 13:43:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './myComponents/Header';
+import Navbar from './myComponents/Navbar.js'
+function App() {
+  return (
+    <div>
+      <Header title="SMR" />
+      <Navbar />
+    </div>
+  );
+}
+export default App;
---
+++ src/myComponents/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <header>
+        <div>
+          <div className='bg-slate-200 flex justify-evenly w-full'>
+            <div className='flex justify-center items-center hidden sm:flex mr-5'  >
+              <div ><strong className='p-2'>Follow us on:</strong></div>
+              <div><a href="#."><i className='bx bxl-instagram icon mr-1 mt-2 '></i></a></div>
+              <div><a href="#."><i className='bx bxl-youtube icon mr-1 mt-2'></i></a></div>
+              <div><a href="#."><i className='bx bxl-twitter icon mr-1 mt-2'></i></a></div>
+              <div><a href="mailto:kunal9313882818@gmail.com"><i className='bx bxl-gmail icon mr-1  mt-2'></i></a></div>
+              <div><strong className='p-1'>|</strong></div>
+              <div><a href="#."><i className='bx bxl-play-store icon mr-1 mt-2'></i></a></div>
+              <div><a href="#."><i className='bx bxl-apple icon mr-1 mt-2'></i></a></div>
+            </div>
+            <div className='flex justify-between'>
+              <div className='flex mr-5'>
+                <div className='mt-1'><a href="#." className='p-1'>हिंदी</a></div>
+                <div><strong className='mt-1' >|</strong></div>
+                <div className='mt-1'><a href="#." className='p-1'>English</a></div>
+              </div>
+              <div className='flex ml-4  '>
+                <div className='mt-1 '><a href="#.">For Passengers</a></div>
+                <div><strong className='p-1'>|</strong></div>
+                <div className='mt-1'><a href="#." >For Corporates</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  )
+}
+
