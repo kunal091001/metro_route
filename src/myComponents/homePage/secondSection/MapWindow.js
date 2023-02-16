678d407a2b61af0d293bf188c9ef12ab224fd8b7
[kunal] <kunal9313882818@gmail.com>
2023-02-16 09:26:00 +0530

    magnifier added

--- src/myComponents/homePage/secondSection/MapWindow.js
+++ src/myComponents/homePage/secondSection/MapWindow.js
@@ -1,12 +1,86 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
 
 import mapImage from "../../../Assets/images/Network_map.jpg";
 
-export default function mapWindow() {
+export default function MapWindow() {
+  var src;
+  var width;
+  var height;
+  var magnifierHeight = 100;
+  var magnifieWidth = 100;
+  var zoomLevel = 2.5;
+
+  const [[x, y], setXY] = useState([0, 0]);
+  const [[imgWidth, imgHeight], setSize] = useState([0, 0]);
+  const [showMagnifier, setShowMagnifier] = useState(false);
+
   return (
     <div className="bg-carbon rounded-2xl border  m-4">
       <h1 className="text-3xl text-black text-center">METRO MAP</h1>
-      <img src={mapImage} alt="" className="rounded-2xl  " />
+      <div
+        style={{
+          position: "relative",
+          height: height,
+          width: width,
+        }}
+      >
+        <img
+          src={mapImage}
+          style={{ height: height, width: width }}
+          onMouseEnter={(e) => {
+            // update image size and turn-on magnifier
+            const elem = e.currentTarget;
+            const { width, height } = elem.getBoundingClientRect();
+            setSize([width, height]);
+            setShowMagnifier(true);
+          }}
+          onMouseMove={(e) => {
+            // update cursor position
+            const elem = e.currentTarget;
+            const { top, left } = elem.getBoundingClientRect();
+
+            // calculate cursor position on the image
+            const x = e.pageX - left - window.pageXOffset;
+            const y = e.pageY - top - window.pageYOffset;
+            setXY([x, y]);
+          }}
+          onMouseLeave={() => {
+            // close magnifier
+            setShowMagnifier(false);
+          }}
+          alt={"img"}
+        />
+
+        <div
+          style={{
+            display: showMagnifier ? "" : "none",
+            position: "absolute",
+
+            // prevent magnifier blocks the mousemove event of img
+            pointerEvents: "none",
+            // set size of magnifier
+            height: `${magnifierHeight}px`,
+            width: `${magnifieWidth}px`,
+            // move element center to cursor pos
+            top: `${y - magnifierHeight / 2}px`,
+            left: `${x - magnifieWidth / 2}px`,
+            opacity: "1", // reduce opacity so you can verify position
+            border: "1px solid lightgray",
+            backgroundColor: "white",
+            backgroundImage: `url('${mapImage}')`,
+            backgroundRepeat: "no-repeat",
+
+            //calculate zoomed image size
+            backgroundSize: `${imgWidth * zoomLevel}px ${
+              imgHeight * zoomLevel
+            }px`,
+
+            //calculate position of zoomed image.
+            backgroundPositionX: `${-x * zoomLevel + magnifieWidth / 2}px`,
+            backgroundPositionY: `${-y * zoomLevel + magnifierHeight / 2}px`,
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
